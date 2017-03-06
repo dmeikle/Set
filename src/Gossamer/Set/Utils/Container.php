500d1d7921103888b4941a283ac235fe671eabb3
Dave Meikle <davemeikle@ymail.com>
2017-03-05 22:06:34 -0800

    removed deprecated features from container

--- src/Gossamer/Set/Utils/Container.php
+++ src/Gossamer/Set/Utils/Container.php
@@ -22,7 +22,7 @@ use Gossamer\Set\Exceptions\ObjectNotFoundException;
 class Container 
 {
 
-    private $directory = null;
+    private $directory = array();
 
     public function __destruct() {
         while (count($this->directory) > 0) {
@@ -36,34 +36,16 @@ class Container
     }
 
     public function get($key) {
-        $directory = $this->getDirectory();
-        if (!array_key_exists($key, $directory)) {
+
+        if (!array_key_exists($key, $this->directory)) {
             throw new ObjectNotFoundException($key . ' does not exist in container');
         }
 
-        $item = $directory[$key];
-
-        if (is_null($item['object'])) {
-            $item['object'] = new $item['objectPath']();
-        }
-
-        $directory[$key] = $item;
-        $this->directory = $directory;
-
-        return $item['object'];
+        return $this->directory[$key];
     }
 
-    private function getDirectory() {
-        if (is_null($this->directory)) {
-            $this->directory = array();
-        }
 
-        return $this->directory;
-    }
-
-    public function set($key, $objectPath, $object = null) {
-        $directory = $this->getDirectory();
-        $directory[$key] = array('objectPath' => $objectPath, 'object' => $object);
-        $this->directory = $directory;
+    public function set($key, $object) {
+        $this->directory[$key] = $object;
     }
 }
